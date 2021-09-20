8fb1cbea9b4f2c5a333ecedf129e571c2c2a61d8
mridulpandey123 <mridul.pandey@jarvis.consulting>
2021-09-20 13:28:30 +0530

    .

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,35 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
 #
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ActionHelloWorld(FormAction):
+
+     def name(self) -> Text:
+         return "designation_form"
+
+     def name(self) -> Text:
+         return "realname_form"
+
+     @staticmethod
+     def required_slots(tracker: Tracker) -> List[Text]:
+         print("required_slots(tracker: Tracker)")
+         return ["realname"]
+
+
+     @staticmethod
+     def required_slots(tracker: Tracker) -> List[Text]:
+         print("required_slots(tracker: Tracker)")
+         return ["designation"]
+
+
+     def submit(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict]:
+
+         dispatcher.utter_message(template="utter_submit")#
+         return []
